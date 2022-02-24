e4db7d95c2f04097a688f21657f5076fb9d12336
Anton Lutsenko <antnetbynet@yandex.com>
2022-02-24 03:03:12 +0300

    add Result component

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,3 +1,13 @@
-export function Results() {
-  return <div>Results</div>;
+import { Thumbnail } from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+export function Results({ results }) {
+  console.log(results);
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex justify-center">
+      {results.map((item) => (
+        <Thumbnail key={item.id} data={item} />
+      ))}
+    </FlipMove>
+  );
 }
